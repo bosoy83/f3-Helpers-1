b25c2b14a1cd4234f74ae8808b33ae4edac996a5
fpumir <fpumir@terredeweb.fr>
2013-01-22 06:57:01 +0100

    original commit
    Signed-off-by: fpumir <fpumir@terredeweb.fr>

---
+++ Helpers/views.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Views class extending native View.
+ *
+ * @package 1.0
+ */
+class views extends View{
+
+ 
+  /** 
+		Convert new lines to paragraph <p>
+		@return string
+		@param $str string
+		@param $addtag string
+	**/
+  function nl2p($str,$addtag='') {
+      return str_replace('<p'.$addtag.'></p>', '', '<p'.$addtag.'>' . preg_replace('#\n|\r#', '</p>$0<p'.$addtag.'>', $str) . '</p>');
+      
+  }
+  
+  /** 
+		Json encode @obj element and format with @format
+		@return string
+		@param $set string
+		@param $format array
+	**/
+  function toJson($obj,$format=array()){
+    $fw=Base::instance();
+    if (isset($_COOKIE[session_name()]))
+			@session_start();
+		$fw->sync('SESSION');
+		$hive=$fw->hive();
+		if(!$obj)
+		  return;
+    if(is_array($obj)){
+      $output=array();
+      foreach($obj as $item){
+        $output[]=$this->_dataSet($format,$item);
+      }
+    } 
+    else
+      $output=$this->_dataSet($format,$obj);
+      
+    if(F3::get('AJAX')&&F3::get('REQUEST.callback')){
+      return F3::get('REQUEST.callback').'('.json_encode($output).')';
+    }
+    return json_encode($output);
+  
+  }
+  
+  protected function _dataSet($format,$item){
+    return array_map(function($elmt) use($item){
+      if(isset($item->$elmt))
+        return $item->$elmt;
+      },$format);
+  }
+  
+}
+
+?>
